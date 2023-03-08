27b837f20cc21eb59c088062aaf14f4bdc972c9e
anreh <anurehka@gmail.com>
2023-03-08 11:27:21 +0800

    added dice image

--- app/src/main/java/com/example/group356practicalw1/MainActivity.kt
+++ app/src/main/java/com/example/group356practicalw1/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.group356practicalw1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
             rollDice()
         }
+        diceImage = findViewById(R.id.dice_image)
     }
         /*
         Click Listener for the Roll Button
@@ -27,6 +30,15 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, "Dice is rolled", Toast.LENGTH_SHORT).show()
         val randomInt = (1..6).random()
 
+        val drawableResource = when(randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
         val resultText: TextView = findViewById(R.id.txtNumber)
 
         resultText.text = randomInt.toString()
